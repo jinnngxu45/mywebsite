da937b69f783127f2b392ef4aed330a0042b5490
“Jing <+>
2021-12-15 17:05:23 -0500

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+
+function addingEventListener() {
+
+    const input = document.getElementById('About');
+
+function clickAlert() {
+    click(['1. Her name is Jing Xu, from China.']
+    ['2. She moved to NYC to continue her Master degree at Parsons School of Design after collage at DePauw University.'] 
+    ['3. Film photography became her everyday getaway, also helped her reconsider her attitude towards art and life.'] 
+    ['4. The practice is more like a meditation, a self-confession, and a time-based ownership recreation to her.'] 
+    ['5. She also does writing, walking, and now coding.']);
+}
+
+input.addEventListener('click', clickAlert);
+
+}
